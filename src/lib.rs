54fdb26880abec26c170a41809de489a9b1bc5cc
grygory zhimolost <grygoryzhimolost'@example.com>
2025-05-28 17:24:19 +0300

    license added

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,20 @@
 #![no_std]
 
+/*
+qserde - a rust rust library for serializing/deserializing ml-kem keys and so on.
+Copyright (C) 2025 grygory zhimolost'
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 //! qserde is a non-std rust library for serializing/deserializing ml-kem(aka kyber) public key, encapsulating/decapsulating 256 bit keys and so on.
 //!## WARNING
 //!Current version of ml-kem crate used in this crate **does not support rand crate of 0.9.x versions**, so if everything doesn't work maybe you should **downgrade the rand crate version** to at least *0.8.5*
